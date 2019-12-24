7930738823d34965c97ab38c7e8e435f08c82fac
yeonuk44 <40634025+yeonuk44@users.noreply.github.com>
2019-12-24 00:24:00 -0800

    Euler_fornula
    with python

---
+++ Euler_formula(with_python).py
@@ -0,0 +1,26 @@
+import math
+
+a = 1.0 # first time
+b = 2.0 # final time
+N = 10 # time term
+alpha = 4.2 # initial value
+
+f = lambda t, y : ((-2 * y) * (1 / t)) + (t * t) # Differential equation
+g = lambda t : ((t * t * t) * (1 / 5)) + (4 * (1 / (t * t))) # Solution equation
+
+h = (b - a) / N
+t = a
+y = alpha
+
+print("Euler formula")
+print("Time \t\t Approximation \t\t Exact_value \t\t error \t\t relative_error")
+print("-------------------------------------------------------------------------------------------")
+print(t,"\t\t",y,"\t\t",y,"\t\t",y-y,"\t\t",y-y)
+
+for i in range(0,N):
+    y = y + (h * (f(t,y)))
+    t = a + ((i + 1) * h)
+    exact = g(t) # Exact_value
+    error = abs(y - exact)
+    relative_error = ((y - exact) / exact) * 100
+    print(t,"\t\t",y,"\t\t",exact,"\t\t",error,"\t\t"relative_error)
